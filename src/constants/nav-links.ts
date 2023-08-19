d5a50e5f5f01c8212af5eeef1b9af49934b9354a
kmfiguerrez <kmfiguerrez@gmail.com>
2023-08-19 09:05:18 +0800

    First version 1 commit.
    - Moved the global.css to styles dir.
    - Initialized a nav component.
    - Created a nav links constant.
    - Set the font to montserrat.

---
+++ src/constants/nav-links.ts
@@ -0,0 +1,18 @@
+const navigationLinks = [
+  {
+    label: 'HOME',
+    href: 'home'
+  },
+  {
+    label: 'ABOUT ME',
+    href: 'about-me'
+  },
+  {
+    label: 'PROJECTS',
+    href: 'projects'
+  },
+  {
+    label: 'CONTACTS',
+    href: 'contacts'
+  },
+]

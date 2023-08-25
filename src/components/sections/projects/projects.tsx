6845c05940943c6da8fb2f5a0976c0b27b126aaa
kmfiguerrez <kmfiguerrez@gmail.com>
2023-08-25 16:37:55 +0800

    Made project cards responsive.

--- src/components/sections/projects/projects.tsx
+++ src/components/sections/projects/projects.tsx
@@ -11,7 +11,7 @@ const ProjectSection = () => {
         PROJECTS
       </h2>
       {/* Project Cards */}
-      <div className="grid grid-cols-3 gap-4 border ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 border ">
         <div>
           <Ipv4Card />
         </div>

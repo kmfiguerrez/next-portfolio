2fa57b07b78512ef8c8a273fa770f05166b77896
kmfiguerrez <kmfiguerrez@gmail.com>
2023-08-31 11:47:58 +0800

    Add socials svg.

--- src/components/sections/contacts/contacts.tsx
+++ src/components/sections/contacts/contacts.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 
 
 const ContactSection = () => {
@@ -7,16 +8,26 @@ const ContactSection = () => {
       <h2 className="text-center text-3xl md:text-5xl font-bold mb-8">
         CONTACTS
       </h2>
-      {/* Flex */}
-      <div className="flex flex-col sm:flex-row sm:justify-between">
-        <div>
+
+      {/* Grid */}
+      <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:gap-x-16">
+
+        <div className="border">
           {/* Sub Heading */}
           <h3 className="text-lg md:text-2xl text-blue-600 mb-8">
             SOCIALS
           </h3>
+          <div className="relative h-[300px] border border-blue-700 sm:w-[380px] sm:mx-auto">
+            <Image 
+              src='/images/socials.svg'
+              alt="Socials Image"
+              fill              
+            />
+          </div>
 
         </div>
-        <div>
+
+        <div className="border">
           {/* Sub Heading */}
           <h3 className="text-lg md:text-2xl text-blue-600 mb-8">
             SEND ME A MESSAGE

caa9d13d080e10bf5db96ab800b050ec53a24801
kmfiguerrez <kmfiguerrez@gmail.com>
2023-09-05 19:31:45 +0800

    Add form component from shadcn.
    - Added schema dir inside the src dir.
    - Added a form-schema ts file inside schema dir.

--- src/components/sections/contacts/form.tsx
+++ src/components/sections/contacts/form.tsx
@@ -1,21 +1,105 @@
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+'use client'
+
+import { type formData, formSchema } from "@/schema/form-schema" 
+import { useForm, SubmitHandler } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { useRef } from "react"
+import { Textarea } from "@/components/ui/textarea"
 
 
 const ContactForm = () => {
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors, isSubmitting },
+  //   reset,    
+  // } = useForm<formData>({
+  //   resolver: zodResolver(formSchema)
+  // })
+  const dataRef = useRef('')
+  const form = useForm<formData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    }
+  })
+
+  const onSubmit: SubmitHandler<formData>  = (data) => {
+    alert(data.name)
+    console.log(data.name)
+    dataRef.current = data.name
+  }
+
   return (
-    <div>
-      Your Name
-      <Input className="rounded-sm mb-4" />
-      Your Email
-      <Input className="rounded-sm mb-4" />
-      Your Message
-      <Textarea className="rounded-sm mb-4" />
-      <Button className="bg-blue-600 hover:bg-blue-800 font-normal tracking-wide text-base">
-        Submit
-      </Button>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+                {form.formState.isSubmitSuccessful.toString()}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+                {form.formState.isSubmitSuccessful.toString()}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='message'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>                
+                <Textarea {...field}/>
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+                {form.formState.isSubmitSuccessful.toString()}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
---
+++ src/schema/form-schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  name: z
+        .string()
+        .min(2, 'Name must be at least 2 characters'),
+  email: z.string().email(),
+  message: z
+            .string()
+            .min(2)
+})
+
+
+
+// Form Inputs Data
+export type formData = z.infer<typeof formSchema>

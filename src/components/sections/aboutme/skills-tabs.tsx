a5b80a20d06d9cce9d1d540db46a7af3d4b0aec7
kmfiguerrez <kmfiguerrez@gmail.com>
2023-11-07 20:03:39 +0800

    Modified the skill tabs.
    - Changed one of the tabs name from networking to infrastructure.

--- src/components/sections/aboutme/skills-tabs.tsx
+++ src/components/sections/aboutme/skills-tabs.tsx
@@ -8,12 +8,12 @@ const SkillsTabs = () => {
     <Tabs defaultValue="coding" className="">
       <TabsList className="md:w-[400px] mx-auto bg-zinc-800 grid grid-cols-2">
         <TabsTrigger value="coding" className="data-[state=active]:bg-zinc-900 data-[state=active]:text-zinc-100">Coding</TabsTrigger>
-        <TabsTrigger value="networking" className="data-[state=active]:bg-zinc-900 data-[state=active]:text-zinc-100">Networking</TabsTrigger>          
+        <TabsTrigger value="infrastructure" className="data-[state=active]:bg-zinc-900 data-[state=active]:text-zinc-100">Infrastructure</TabsTrigger>          
       </TabsList>
       <TabsContent value="coding" className="mt-8">
         <CodingSKills />
       </TabsContent>
-      <TabsContent value="networking" className="mt-8">
+      <TabsContent value="infrastructure" className="mt-8">
         <NetworkingSkills />
       </TabsContent>        
     </Tabs>
